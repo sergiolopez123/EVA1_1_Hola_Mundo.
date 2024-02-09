bd43fefa755c6f28f11f0c74984e3a9611f52797
sergiolopez123 <159494442+sergiolopez123@users.noreply.github.com>
2024-02-09 16:54:01 -0600

    Add files via upload

---
+++ eva1_2_identificadores/Eva1_2_identificadores.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_identificadores;
+
+/**
+ *
+ * @author jairl
+ */
+public class Eva1_2_identificadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Variables:
+        // tipos de dato + nombre de identificador
+        // short int byte long--> enteros
+        // float double --> numeros con decimal
+        // char -->character --> caracter
+        // boolean --> verdero o falso
+        // string --> cadena de texto
+        // si no se declara una variable, no se puede usar.
+        int Edad; // declaracion de una variable
+        String Nombre;
+        double salario;
+        
+        // Asignacion de valores
+        // Identificador = valor
+        // se coloca el valor 50 dentro de la variable edad
+        Edad = 50;
+        Nombre = "Sergio Lopez Reza";
+        salario = 200000.23;
+        // como los usamos:
+        System.out.println (Nombre);
+        System.out.println (Edad);
+        System.out.println (salario); 
+    } }
+    
