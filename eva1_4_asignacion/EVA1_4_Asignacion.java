76a66e1fca02e009d3936d471676619e7f9d4fcd
sergiolopez123 <159494442+sergiolopez123@users.noreply.github.com>
2024-02-15 16:45:14 -0600

    Add files via upload

---
+++ eva1_4_asignacion/EVA1_4_Asignacion.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_asignacion;
+
+/**
+ *
+ * @author jairl
+ */
+public class EVA1_4_Asignacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int EDAD; //declaracion
+        EDAD = 21;
+        System.out.println("mi edad es:");
+        System.out.println(EDAD);
+        String Nombre;
+        Nombre = "Sergio Lopez";
+        System.out.println("mi nombre es:");
+        System.out.println(Nombre);
+        String Carrera;
+        Carrera = "Ingieneria en sistmeas";
+        System.out.println("mi carrera es:");
+        System.out.println(Carrera);
+                
+    }
+    
+}
